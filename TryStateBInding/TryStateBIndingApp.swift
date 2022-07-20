786ba69d9da3ed0217fafb90fd9caea679091a5f
Ardhan12 <arief.ramadhan43@gmail.com>
2022-07-20 15:50:36 +0700

    Initial Commit

---
+++ TryStateBInding/TryStateBIndingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryStateBIndingApp.swift
+//  TryStateBInding
+//
+//  Created by Arief Ramadhan on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TryStateBIndingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
